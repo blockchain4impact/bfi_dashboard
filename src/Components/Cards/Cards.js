2a9fe2e6e0dc481f86bee7e0e6a391d7d4ff43a7
giridhar <giribala2001@gmail.com>
2023-12-08 20:43:58 +0530

    data populated

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -6,35 +6,41 @@ export default function Cards({ data }) {
 
   if (!data || !data.items) {
     return <div>Loading...</div>;
-  }  
+  }
   return (
     <div className="cards">
-        <div className="card-header">
-            <div className="rectangle">
-                <img src={Vector} alt="vector-img" />
-            </div>
+      <div className="card-header">
+        <div className="rectangle">
+          <img src={Vector} alt="vector-img" />
         </div>
-        <div className="card-body">
-            <div className="card-head">
-                {data.title}
-            </div>
-            {data.items.map(item => (
-              <div className="card-content" key={item.id}>
-                  <div className="card-percentage">
-                      <p>{item.percentage}</p>
-                  </div>
-                  <div className="card-body-content">
-                      <p>{item.title}</p>
-                      <p>{item.detail}</p>
-                  </div>
-                  <div className="names-container">
-                    <div className="rounded-name">
-                        <p>{item.names}</p>
-                    </div>
-                  </div>
+      </div>
+      <div className="card-body">
+        <div className="card-head">
+          {data.title}
+        </div>
+        <div className='card-container'>
+          {data.items.map(item => (
+            <div className="card-content" key={item.id}>
+              <div className="card-percentage">
+                <p>{item.percentage}</p>
               </div>
-            ))}
+              <div className="card-body-content">
+                <div className='card-details'>
+                  <p>{item.title}</p>
+                  <p>{item.detail}</p>
+                </div>
+                <div className="names-container">
+                  {item.names.map((names) =>
+                    <div className="rounded-name">
+                      <p>{names}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
     </div>
   );
 }
