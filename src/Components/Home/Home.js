73161015338dd92a3aecc52d7205923b5dc111ca
tmbc12 <reachus@tmbc.in>
2023-12-21 13:29:22 +0530

    changes for client review

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Cards from "../Cards/Cards";
 import "../Home/Home.css";
@@ -10,9 +11,9 @@ export const cardData = [
     items: [
       {
         id: 1,
-        percentage: "12%",
+        percentage: "25%",
         title:
-          "To be recognized as an Indian Biomedical Innovation Ecosystem enabler",
+          "BFI-BIOME(Virtual Network)",
         detail: "BFI is recognized as a key catalyst supporter for Biome",
         names: ["Pooja", "Gaurav"],
         bgcolor: 'rgb(138,43,226)',
@@ -20,9 +21,9 @@ export const cardData = [
       },
       {
         id: 2,
-        percentage: "100%",
+        percentage: "0%",
         title:
-          "Launch BFI-BIOME Network and discussions with Institutes and Incubators",
+          "National Conferences 2024",
         detail: "Successful launch with 10+ EoI received",
         names: ["Pooja", "Gaurav"],
         bgcolor: 'rgb(255,140,0)',
@@ -102,7 +103,7 @@ export const cardData = [
       {
         id: 1,
         percentage: "75%",
-        title: "Hiring: 3 members in BRI, 7 in IDFS and 2 in BFI Support roles",
+        title: "Interviews and Finalization",
         detail: "Complete interviews and Finalise Candidates - BRI",
         names: ["Pooja and Faraz", "Gaurav"],
         bgcolor: 'rgb(138,43,226)',
@@ -195,8 +196,8 @@ export const cardData = [
     items: [
       {
         id: 1,
-        percentage: "35%",
-        title: "Hire core IDFS team",
+        percentage: "50%",
+        title: "Interviews and finalization",
         detail: "Complete interviews and Finalise Candidates",
         names: ["Ramnath", "Naga"],
         bgcolor: 'rgb(138,43,226)',
@@ -205,10 +206,10 @@ export const cardData = [
       },
       {
         id: 2,
-        percentage: "0%",
-        title: "Launch HCD Fellowship",
+        percentage: "25%",
+        title: "Contract signing with BVT",
         detail: "Complete Onboarding",
-        names: ["VSR - Knowledge and Research Lead", "Naga"],
+        names: ["Ramnath", "Naga"],
         bgcolor: 'rgb(255,140,0)',
         lightbgcolor: 'rgba(255,140,0,0.2)',
         width: '99%'
@@ -243,3 +244,250 @@ export default function Home() {
     </div>
   );
 }
+
+
+// import React from "react";
+// import Cards from "../Cards/Cards";
+// import "../Home/Home.css";
+
+// export const cardData = [
+//   {
+//     id: 1,
+//     title: "BRI",
+//     pathname: '/bri',
+//     items: [
+//       {
+//         id: 1,
+//         percentage: "12%",
+//         title:
+//           "To be recognized as an Indian Biomedical Innovation Ecosystem enabler",
+//         detail: "BFI is recognized as a key catalyst supporter for Biome",
+//         names: ["Pooja", "Gaurav"],
+//         bgcolor: 'rgb(138,43,226)',
+//         lightbgcolor: 'rgba(138,43,226,0.2)',
+//       },
+//       {
+//         id: 2,
+//         percentage: "100%",
+//         title:
+//           "Launch BFI-BIOME Network and discussions with Institutes and Incubators",
+//         detail: "Successful launch with 10+ EoI received",
+//         names: ["Pooja", "Gaurav"],
+//         bgcolor: 'rgb(255,140,0)',
+//         lightbgcolor: 'rgba(255,140,0,0.2)',
+//       },
+//       {
+//         id: 3,
+//         percentage: "58%",
+//         title: "Signing MoU with Insitutes and Incubators for BFI-BIOME",
+//         detail: "Signing of MoU(s) with 5+ Institute/Incubators",
+//         names: ["Pooja", "Gaurav"],
+//         bgcolor: 'rgb(50,205,50)',
+//         lightbgcolor: 'rgba(50,205,50,0.2)',
+//       },
+//       {
+//         id: 4,
+//         percentage: "0%",
+//         title: "Finalizing projects/programs and commencement of the projects/programs",
+//         detail: "Commencement of Projects/programs",
+//         names: ["Pooja", "Gaurav"],
+//       },
+//       {
+//         id: 5,
+//         percentage: "0%",
+//         title: "Periodic project/program review/evaluation",
+//         detail: "Review reports/results",
+//         names: ["Pooja", "Gaurav"],
+//       },
+//       {
+//         id: 6,
+//         percentage: "0%",
+//         title: "Annual Project/Program Review",
+//         detail: "Impact report",
+//         names: ["Pooja", "Gaurav"],
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     title: "BIOME",
+//     pathname: '/biome',
+//     items: [
+//       {
+//         id: 1,
+//         percentage: "25%",
+//         title: "Successful launch with 10+ Eol recieved",
+//         detail: "Discussions with Inst./Incubators",
+//         names: ["Girish", "Gaurav"],
+//         bgcolor: 'rgb(138,43,226)',
+//         lightbgcolor: 'rgba(138,43,226,0.2)',
+//       },
+//       {
+//         id: 2,
+//         percentage: "75%",
+//         title: "Signing of MoU(s) with 5+ Institute/Incubators",
+//         detail: "Finalize the list of Institutes/Incubators",
+//         names: ["Faraz", "Naga"],
+//         bgcolor: 'rgb(255,140,0)',
+//         lightbgcolor: 'rgba(255,140,0,0.2)',
+//       },
+//       {
+//         id: 3,
+//         percentage: "0%",
+//         title: "Commencement of Projects/programs",
+//         detail: "Shortlisting of projects/finalization of program",
+//         names: ["Girish", "Gaurav"],
+//         bgcolor: 'rgb(50,205,50)',
+//         lightbgcolor: 'rgba(50,205,50,0.2)',
+//       },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     title: "ORG",
+//     pathname: '/org',
+//     items: [
+//       {
+//         id: 1,
+//         percentage: "75%",
+//         title: "Hiring: 3 members in BRI, 7 in IDFS and 2 in BFI Support roles",
+//         detail: "Complete interviews and Finalise Candidates - BRI",
+//         names: ["Pooja and Faraz", "Gaurav"],
+//         bgcolor: 'rgb(138,43,226)',
+//         lightbgcolor: 'rgba(138,43,226,0.2)',
+//         taskcard: ["Interviews and Finalization", "Contract signing with BVT", ""]
+//       },
+//       {
+//         id: 2,
+//         percentage: "60%",
+//         title: "Clean audit with PWC",
+//         detail: "Interium Audit Report",
+//         names: ["Girish", "Gaurav"],
+//         bgcolor: 'rgb(255,140,0)',
+//         lightbgcolor: 'rgba(255,140,0,0.2)',
+//       },
+//       {
+//         id: 3,
+//         percentage: "75%",
+//         title: "Infrastructure",
+//         detail: "Öffice space",
+//         names: ["Team BFI", "Naga"],
+//         bgcolor: 'rgb(50,205,50)',
+//         lightbgcolor: 'rgba(50,205,50,0.2)',
+//       },
+//       {
+//         id: 4,
+//         percentage: "100%",
+//         title: "Hiring External Partners",
+//         detail: "The Madras Branding Company",
+//         names: ["Faraz", "Gaurav"],
+//       },
+//       {
+//         id: 5,
+//         percentage: "90%",
+//         title: "PR and Social Media Promotions",
+//         detail: "Brand Building",
+//         names: ["Faraz", "Naga"],
+//       },
+//       {
+//         id: 6,
+//         percentage: "50%",
+//         title: "Website updation",
+//         detail: "Visibility and Brand Building",
+//         names: ["Girish", "Naga"],
+//       },
+//       {
+//         id: 7,
+//         percentage: "25%",
+//         title: "Print Media Activities and PR",
+//         detail: "Goals and Milestones setting",
+//         names: ["Faraz", "Naga"],
+//       },
+//       {
+//         id: 8,
+//         percentage: "75%",
+//         title: "Dashboard creation",
+//         detail: "Goals and Milestones setting",
+//         names: ["Faraz", "Naga"],
+//       },
+//       {
+//         id: 9,
+//         percentage: "75%",
+//         title: "Procurements and Vendor Payments",
+//         detail: "Smooth operations",
+//         names: ["Faraz", "Naga"],
+//       },
+//       {
+//         id: 10,
+//         percentage: "16%",
+//         title: "Onboarding a Grant/ Project management platform",
+//         detail: "Streamlined Process and Documentation",
+//         names: ["Divyarth", "Naga"],
+//       },
+//       {
+//         id: 11,
+//         percentage: "75%",
+//         title: "Closure of projects under Crypto Relief",
+//         detail: "All relief projects are closed and audit is completed",
+//         names: ["Divyarth", "Naga"],
+//       },
+//     ],
+//   },
+//   {
+//     id: 4,
+//     title: "DFS",
+//     pathname: '/dfs',
+//     percentage: "75%",
+//     subtasks: 'Contract signing with BVT',
+//     taskslist: ['BFI-BIOME(Virtual Network)', 'National Conferences 2024'],
+//     items: [
+//       {
+//         id: 1,
+//         percentage: "35%",
+//         title: "Hire core IDFS team",
+//         detail: "Complete interviews and Finalise Candidates",
+//         names: ["Ramnath", "Naga"],
+//         bgcolor: 'rgb(138,43,226)',
+//         lightbgcolor: 'rgba(138,43,226,0.2)',
+//         width: '66%'
+//       },
+//       {
+//         id: 2,
+//         percentage: "0%",
+//         title: "Launch HCD Fellowship",
+//         detail: "Complete Onboarding",
+//         names: ["VSR - Knowledge and Research Lead", "Naga"],
+//         bgcolor: 'rgb(255,140,0)',
+//         lightbgcolor: 'rgba(255,140,0,0.2)',
+//         width: '99%'
+//       },
+//       {
+//         id: 3,
+//         percentage: "20%",
+//         title: "Launch IHCR Fund",
+//         detail: "Onboarding the Partners Process",
+//         names: ["Ramnath", "Naga"],
+//         bgcolor: 'rgb(50,205,50)',
+//         lightbgcolor: 'rgba(50,205,50,0.2)',
+//         width: '80%'
+//       },
+//       {
+//         id: 4,
+//         percentage: "45%",
+//         title: "Regular Program Monitoring + Team Governance",
+//         detail: "Onboarding the Partners Process",
+//         names: ["Ramnath", "Naga"],
+//       },
+//     ],
+//   },
+// ];
+
+// export default function Home() {
+//   return (
+//     <div className="home">
+//       {cardData.map((card) => (
+//         <Cards key={card.id} data={card} />
+//       ))}
+//     </div>
+//   );
+// }
--- src/Components/TaskCards/TaskCards.js
+++ src/Components/TaskCards/TaskCards.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "../TaskCards/TaskCards.css";
 import { MoreOutlined } from "@ant-design/icons";
-import { cardData } from "../Home/Home";
-
 export default function TaskCards(props) {
+
+  const cardColors = ['#0072BC', '#FCBE04'];
+
   return (
     <div className="task-cards">
       <div className="task-cards-header">
         <p style={{ margin: 0 }}>{props.title}</p>
         <MoreOutlined />
       </div>
-      {props.task.map(data => (
-        <>
-          <div className="task-cards-body">
-            <div className="task-card-data">
-              <div className="user-avatar">{data.slice(0,1)}</div>
-              <span>{data}</span>
+      {props.task.map((data, index) => (
+        <div className="task-cards-body" key={index}>
+          <div className="task-card-data" style={{ borderColor: cardColors[index] }}>
+            <div className="user-avatar" style={{ background: cardColors[index] }}>
+              {data.slice(0, 1)}
             </div>
+            <span>{data}</span>
           </div>
-        </>
+        </div>
       ))}
 
       <div className="task-card-progress-bar">
@@ -26,7 +27,41 @@ export default function TaskCards(props) {
         <div className="progress-bar" style={{ width: props.percentage }}></div>
       </div>
     </div>
-
-
   );
 }
+
+
+
+
+// import React from "react";
+// import "../TaskCards/TaskCards.css";
+// import { MoreOutlined } from "@ant-design/icons";
+// import { cardData } from "../Home/Home";
+
+// export default function TaskCards(props) {
+//   return (
+//     <div className="task-cards">
+//       <div className="task-cards-header">
+//         <p style={{ margin: 0 }}>{props.title}</p>
+//         <MoreOutlined />
+//       </div>
+//       {props.task.map(data => (
+//         <>
+//           <div className="task-cards-body">
+//             <div className="task-card-data">
+//               <div className="user-avatar">{data.slice(0,1)}</div>
+//               <span>{data}</span>
+//             </div>
+//           </div>
+//         </>
+//       ))}
+
+//       <div className="task-card-progress-bar">
+//         <div className="progress-title">Progress <span> {props.percentage} </span></div>
+//         <div className="progress-bar" style={{ width: props.percentage }}></div>
+//       </div>
+//     </div>
+
+
+//   );
+// }
