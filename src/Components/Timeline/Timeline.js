c77f65af279d918ddfe359ee83df1f055ed015be
giridhar <giribala2001@gmail.com>
2024-01-22 22:11:07 +0530

    changes

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -2,59 +2,114 @@ import React from 'react';
 
 const Timeline = () => {
     const years = [];
-    const startYear = 2022
-    const endYear = 2023
     for (let i = 2022; i <= 2023; i++) {
         years.push(i);
     }
-    const tasks = [
-        {
-            name: 'Launch BPI-BIOME Network and discussion with Institutes and Incubators',
-            start: 2023,
-            end: 2024,
-            progress: 100,
-            color: 'green'
-        },
-        {
-            name: 'Signing MoU with Institutes and Incubators for BPI-BIOME',
-            start: 2024,
-            end: 2025,
-            progress: 87,
-            color: 'yellow'
-        },
-        {
-            name: 'Finalising projects/programs and commencement of the projects/program',
-            start: 2025,
-            end: 2026,
-            progress: 0,
-            color: 'red'
-        },
-        // Add more tasks as needed
-    ];
+    const oct = 50, nov = 250, dec = 450, janmar = 700, aprjun = 900, julsep = 1100, octdec = 1300;
 
+    const data = [{
+        title: 'Hire core IDFS team',
+        names: ['N', 'G'],
+        startDate: oct,
+        endDate: dec,
+        progress: '35%',
+        year: 2023
+    },
+    {
+        title: 'Launch HCD Fellowship',
+        names: ['N'],
+        startDate: oct,
+        endDate: janmar,
+        progress: '0%',
+        year: 2024
+    }, {
+        title: 'Launch IHCR Fund',
+        names: ['R', 'N'],
+        startDate: oct,
+        endDate: dec,
+        progress: '30%',
+        year: 2023
+    }, {
+        title: 'Regular Program Monitoring + Team Governance',
+        names: ['R', 'N'],
+        startDate: oct,
+        endDate: janmar,
+        progress: '45%',
+        year: 2024
+    }]
 
-    const progressStart = ((5 - 2023) / (2023 - 2022)) * 100;
-    const progressEnd = ((25 - 2022) / (2023 - 2022)) * 100;
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     return (
-        <svg style={{ width: '100%', height: `${50 + tasks.length * 30}px` }}>
-            {months.map((month, index) => (
-                <text key={month} x={`${(index / 12) * 100}%`} y="15" textAnchor="middle" style={{ fontSize: '10px' }}>{month}</text>
-            ))}
-            <line x1="10" y1="50" x2="90%" y2="50" stroke="#000" strokeWidth="2" />
-            {years.map((year, index) => (
-                <g key={year} transform={`translate(${10 + index * 80 / (years.length - 1)}%, 0)`}>
-                    <line x1="0" y1="45" x2="0" y2="55" stroke="#000" strokeWidth="2" />
-                    <text x="0" y="40" textAnchor="middle" style={{ fontSize: '12px' }}>{year}</text>
+        <svg viewBox="0 0 1500 1500" preserveAspectRatio="none">
+            <g>
+                <text x="265" y="25"
+                    fill="rgba(78, 91, 110, 1)" dx="-11.1953125px" font-weight="700" font-size='14px'>2023</text>
+
+                <text x="50" y="55" id="oct"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Oct</text>
+                <line x1="50" x2="50" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+                <text x="250" y="55"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Nov</text>
+                <line x1="250" x2="250" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+                <text x="450" y="55" id="dec"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Dec</text>
+                <line x1="450" x2="450" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+                <line x1="615" x2="615" y1="0" y2="25" fill="none" stroke="rgba(118, 131, 150, 1)" stroke-width="1"></line>
+
+                <text x="965" y="25"
+                    fill="rgba(78, 91, 110, 1)" dx="-11.1953125px" font-weight="700" font-size='14px'>2024</text>
+
+                <text x="700" y="55" id="oct"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Jan - Mar</text>
+                <line x1="725" x2="725" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+                <text x="900" y="55"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Apr - Jun</text>
+                <line x1="925" x2="925" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+                <text x="1100" y="55" id="dec"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Jul - Sep</text>
+                <line x1="1125" x2="1125" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+                <text x="1300" y="55" id="dec"
+                    fill="rgba(118, 131, 150, 1)" font-weight="600" dx="-11.1953125px">Oct - Dec</text>
+                <line x1="1325" x2="1325" y1="75" y2="1155" fill="none" stroke="rgba(235, 237, 244, 1)" stroke-width="1"></line>
+
+            </g>
+            {data.map((value, i) => (
+                <g>
+                    {value.names?.map((name, i) => (
+                        <svg x={oct} y={`10 * ${i + 1}`}>
+                            <circle cx='50' cy="50" r="7" fill="rgba(142, 205, 249, 1)" />
+                            <text x="82.5" y="94" fill="rgba(80, 80, 80, 1)" fontSize={'10px'} font-weight="600" dx="-11.1953125px">
+                                {name}
+                            </text>
+
+
+                        </svg>
+                    ))}
+                    <text x={oct * 2} y={`100` * `${i + 1}` - 5} fill="rgba(118, 131, 150, 1)" fontSize={'12px'} font-weight="600" dx="-11.1953125px">
+                        {value.title}
+                    </text>
+                    <rect x={value.startDate} y={`100` * `${i + 1}`} width={value.year === 2023 ? value.endDate - value.startDate : value.endDate - value.startDate + 25} height="45" rx="25" ry="25" fill="rgba(253, 212, 212, 1)"></rect>
+                    <rect x={value.startDate} y={`100` * `${i + 1}`} width="87.5" height="45" rx="25" ry="25" fill="rgba(243, 87, 87, 1)"></rect>
+                    <circle cx={value.startDate * 1.5} cy={`100` * `${i + 1}` + 22} r="7" fill="rgba(195, 72, 72, 1)" />
+                    <circle cx={value.startDate * 1.5} cy={`100` * `${i + 1}` + 22} r="3" fill="#FFF" />
+                    <svg x={value.year === 2023 ? value.endDate - value.startDate + 60 : value.endDate - value.startDate + 85} y={`100` * `${i + 1}` + 8}>
+                        <filter id="shadow">
+                            <feDropShadow dx="0.2" dy="0.4" stdDeviation="0.2" />
+                        </filter>
+                        <rect filter="url(#shadow)" x='0' y='0' width="49px" height="28px" rx="14" ry="14" fill="#FFFF"></rect>
+                        <text x='23.5' y='18' fill="black" fontSize={'12px'} font-weight="600" dx="-11.1953125px">
+                            {value.progress}
+                        </text>
+                    </svg>
                 </g>
+
             ))}
-            {tasks.map((task, index) => (
-                <g key={task.name} transform={`translate(${10 + ((task.start - startYear) / (endYear - startYear)) * 80}%, ${70 + index * 30})`}>
-                    <text x="0" y="-5" style={{ fontSize: '12px' }}>{task.name} ({task.progress}%)</text>
-                    <rect x="0" y="0" width={`${((task.end - task.start) / (endYear - startYear)) * 80}%`} height="10" fill={task.color} />
-                </g>
-            ))}
-        </svg>
+        </svg >
     );
 };
 
