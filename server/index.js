30e953ee040ad527cb23d1ce61e51b56067ad7d2
GiridharHUB <scintillantmail@gmail.com>
2024-02-02 18:26:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(
   })
 )
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", allowedDomains);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if ('OPTIONS' == req.method) {
