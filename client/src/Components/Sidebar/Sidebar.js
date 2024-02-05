3d318a0a10f20f1754f19bbd28d43a07a47969b1
giridhar <giribala2001@gmail.com>
2024-02-05 23:01:59 +0530

    complete

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -6,7 +6,7 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-menu">
-          <NavLink to='/dashboard' activeclassname='active'>Dashboard</NavLink>
+          <NavLink to='/' activeclassname='active'>Dashboard</NavLink>
           <NavLink to='/bri'>BRI</NavLink>
           <NavLink to='/biome'>BIOME</NavLink>
           <NavLink to='/org'>ORG</NavLink>
